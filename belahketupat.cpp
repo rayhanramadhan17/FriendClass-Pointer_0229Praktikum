3ec0556c1bbb692225d60d4509cf71148e6bb867
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-27 20:41:09 +0700

    member layang layang

--- belahketupat.cpp
+++ belahketupat.cpp
@@ -4,3 +4,19 @@ using namespace std;
 class BelahKetupat; // forward declaration
 
 class LayangLayang {
+private:
+    double d1, d2, s1, s2;
+
+public:
+    void input() {
+        cout << "Input diagonal 1 Layang-Layang: ";
+        cin >> d1;
+        cout << "Input diagonal 2 Layang-Layang: ";
+        cin >> d2;
+        cout << "Input sisi 1 Layang-Layang: ";
+        cin >> s1;
+        cout << "Input sisi 2 Layang-Layang: ";
+        cin >> s2;
+    }
+
+   
